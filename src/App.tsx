bdb26bbbab378b94314fb248bbd5e8e772a36359
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2024-03-09 18:45:04 -0800

    add search and clippings to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,46 @@
 import Viewer from "@samvera/clover-iiif/viewer";
+import {
+  InformationPanel,
+  AnnotationEditor,
+  EditorProvider,
+} from "../lib/main";
 
 function App() {
   const url = "http://localhost:3000";
 
   return (
     <>
-      <Viewer
-        iiifContent={`${url}/api/newspaper/collection`}
-      />
+      <EditorProvider>
+        <Viewer
+          iiifContent={`${url}/api/newspaper/collection`}
+          iiifContentSearch={`${url}/api/newspaper_search/1?q=Berliner`}
+          plugins={[
+            {
+              id: "AnnotationEditor",
+              menu: {
+                component: AnnotationEditor,
+                componentProps: {
+                  annotationServer: `${url}/api/annotationsByCanvas/1`,
+                  token: "123abc",
+                },
+              },
+              informationPanel: {
+                component: InformationPanel,
+                label: { none: ["Clippings"] },
+                componentProps: {
+                  annotationServer: `${url}/api/annotations/1`,
+                  token: "123abc",
+                },
+              },
+            },
+          ]}
+          options={{
+            ignoreAnnotationOverlaysLabels: ["Clippings"],
+            informationPanel: { open: true, renderAbout: false },
+            canvasHeight: "640px",
+          }}
+        />
+      </EditorProvider>
     </>
   );
 }
