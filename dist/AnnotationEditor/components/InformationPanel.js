83cc2138e65a023fdbaed152c770d20825a39767
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2024-03-10 23:22:40 -0700

    add dist

--- dist/AnnotationEditor/components/InformationPanel.js
+++ dist/AnnotationEditor/components/InformationPanel.js
@@ -1,53 +1,62 @@
 import "../../assets/InformationPanel.css";
-import { jsxs as v, jsx as e } from "react/jsx-runtime";
-import { useState as o, useEffect as A } from "react";
-import { useEditorState as C } from "../context/annotation-editor-context.js";
-import b from "./AnnotationItem.js";
-const x = "_container_1tymf_1", y = {
-  container: x
-}, I = ({
-  annotations: r,
-  activeManifest: a,
-  canvas: s,
-  viewerConfigOptions: c,
-  openSeadragonViewer: p,
-  useViewerDispatch: l,
-  useViewerState: m,
-  token: d,
-  annotationServer: f
+import { jsxs as C, jsx as e } from "react/jsx-runtime";
+import { useState as s, useEffect as c } from "react";
+import { useEditorState as y } from "../context/annotation-editor-context.js";
+import T from "./AnnotationItem.js";
+const b = "_container_1tymf_1", v = {
+  container: b
+}, E = ({
+  annotations: p,
+  activeManifest: l,
+  canvas: h,
+  viewerConfigOptions: m,
+  openSeadragonViewer: d,
+  useViewerDispatch: f,
+  useViewerState: u,
+  token: i,
+  annotationServer: o
 }) => {
-  const [h, u] = o(), [n, g] = o(r), j = C(), { clippingsUpdatedAt: i } = j;
-  return A(() => {
-    i && fetch(f, {
+  const [g, j] = s(), [n, r] = s(p), A = y(), { clippingsUpdatedAt: a } = A;
+  return c(() => {
+    fetch(o, {
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${d}`
+        Authorization: `Bearer ${i}`
       }
     }).then((t) => t.json()).then((t) => {
-      g(t.items);
+      r(t.items);
     });
-  }, [i]), /* @__PURE__ */ v("div", { className: y.container, children: [
+  }, []), c(() => {
+    a && fetch(o, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${i}`
+      }
+    }).then((t) => t.json()).then((t) => {
+      r(t.items);
+    });
+  }, [a]), /* @__PURE__ */ C("div", { className: v.container, children: [
     /* @__PURE__ */ e("p", { children: "Use the button to make a clipping. Clippings from this record is available below." }),
     /* @__PURE__ */ e("p", { children: /* @__PURE__ */ e("a", { href: "", children: "View all clippings." }) }),
     /* @__PURE__ */ e("p", { children: "Clippings from this record" }),
     n == null ? void 0 : n.map((t) => /* @__PURE__ */ e(
-      b,
+      T,
       {
         annotation: t,
-        activeManifest: a,
-        canvas: s,
-        viewerConfigOptions: c,
-        openSeadragonViewer: p,
-        useViewerState: m,
-        useViewerDispatch: l,
-        activeTarget: h,
-        setActiveTarget: u
+        activeManifest: l,
+        canvas: h,
+        viewerConfigOptions: m,
+        openSeadragonViewer: d,
+        useViewerState: u,
+        useViewerDispatch: f,
+        activeTarget: g,
+        setActiveTarget: j
       },
       t.id
     ))
   ] });
 };
 export {
-  I as InfomationPanel,
-  I as default
+  E as InfomationPanel,
+  E as default
 };
