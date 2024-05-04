5113c830c6d6e06b535448157af1f6f12cdd67aa
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2024-05-03 17:17:34 -0700

    update variables on <App>

--- src/App.tsx
+++ src/App.tsx
@@ -7,40 +7,40 @@ import {
 
 function App() {
   const base_url = "http://localhost:3000";
+  const iiifContent = `${base_url}/api/fixtures/newspaper/issue_1`;
   const annotationServer = `${base_url}/api/annotations/1`;
   const token = "123abc";
+  const searchTerm = "Berliner";
 
   return (
-    <>
-      <EditorProvider>
-        <Viewer
-          iiifContent={`${base_url}/api/fixtures/newspaper/issue_1`}
-          iiifContentSearchQuery={{ q: "Berliner" }}
-          plugins={[
-            {
-              id: "AnnotationEditor",
-              imageViewer: {
-                menu: {
-                  component: AnnotationEditor,
-                  componentProps: {
-                    annotationServer: annotationServer,
-                    token: token,
-                  },
-                },
-              },
-              informationPanel: {
-                component: InformationPanel,
-                label: { none: ["Clippins"] },
+    <EditorProvider>
+      <Viewer
+        iiifContent={iiifContent}
+        iiifContentSearchQuery={{ q: searchTerm }}
+        plugins={[
+          {
+            id: "AnnotationEditor",
+            imageViewer: {
+              menu: {
+                component: AnnotationEditor,
                 componentProps: {
                   annotationServer: annotationServer,
                   token: token,
                 },
               },
             },
-          ]}
-        />
-      </EditorProvider>
-    </>
+            informationPanel: {
+              component: InformationPanel,
+              label: { none: ["Clippins"] },
+              componentProps: {
+                annotationServer: annotationServer,
+                token: token,
+              },
+            },
+          },
+        ]}
+      />
+    </EditorProvider>
   );
 }
 
